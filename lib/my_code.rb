ca43713fa970cfafadda22bcd3899bbb476337fc
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-19 05:06:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,12 @@ def map(array)
   end
   new_array
 end
+
+def reduce(array, starting_point = nil)
+  total = 0 
+  if starting_point
+    total = starting_point
+  end
+  i = 0 
+  while i < array.length do
+    array
